25d8e019faa957e65b14f2d396f92148425a71f5
RohitYelnare <ryelnare@gmail.com>
2023-03-25 16:24:22 +0530

    db connect now from app.js

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mysql from 'mysql2';
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,5 +9,21 @@ app.use(cors());
 const port = 3000;
 
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3300,
+    database: 'fattoslimdb',
+    multipleStatements: true
+});
+
+connection.connect((err)=> {
+    if(!err)
+        console.log('Connection Established Successfully');
+    else
+        console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+});
+
+connection.end();
 
 app.listen(port, ()=>{console.log(`app is listening on ${port}` )});
--- backend/model/client.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: 3300,
-    database: 'fattoslimdb',
-    multipleStatements: true
-});
