c39382e75ef647be3d07028b7425f80f6c10ca9c
Neel Majethiya <neel.b.majethiya@gmail.com>
2023-03-25 21:06:08 +0530

    commit 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import ClientRegistration from './components/ClientRegistration';
 import FranchiseRegistration from './components/FranchiseRegistration';
 import OtpVerification from './components/OtpVerification';
+import Home from './components/Home';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/register/client" element={<ClientRegistration />} />
         <Route path="/register/franchise" element={<FranchiseRegistration />} />
         <Route path="/OTPverification" element={<OtpVerification />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
     </>
--- frontend/src/components/ClientRegistration.jsx
+++ frontend/src/components/ClientRegistration.jsx
@@ -1,16 +1,12 @@
-import {FormControl,FormLabel,Radio ,RadioGroup } from '@mui/material';
+import {FormLabel,Radio ,RadioGroup } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useSelector, useDispatch } from 'react-redux'
 import { setClientData } from '../store/clientStore'
 import { useState } from 'react';
